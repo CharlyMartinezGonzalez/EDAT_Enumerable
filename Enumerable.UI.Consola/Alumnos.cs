34c7c23557ce8e34740211c05c0837aace088661
CharlyMartinezGonzalez <23030392@itesa.edu.mx>
2024-11-21 10:53:11 -0600

    version 01

---
+++ Enumerable.UI.Consola/Alumnos.cs
@@ -0,0 +1,15 @@
+﻿namespace Enumerable.UI.Consola
+{
+    public class Alumnos
+    {
+        public int Matricula {  get; set; }
+        public String? Nombre {  get; set; }
+        public decimal Precio {  get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Matricula}\t{Nombre}\t{Precio:c2}";
+        }
+    }
+}
---
+++ Enumerable.UI.Consola/Program.cs
@@ -0,0 +1,53 @@
+﻿using Enumerable.UI.Consola;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Enumerable");
+        Console.WriteLine("===========================================");
+        Console.WriteLine("\n");
+        Console.WriteLine("*Mtc*   *Lista*  *precio*  List<T>");
+
+   
+
+
+        #region Lista
+        var Lista = new List<Alumnos>
+        {
+            new Alumnos {Matricula = 1, Nombre = "Juan", Precio = 24.459M},
+            new Alumnos {Matricula = 2, Nombre = "jose", Precio = 22.563M},
+            new Alumnos {Matricula = 3, Nombre = "Luis", Precio = 21.999M},
+            new Alumnos {Matricula = 4, Nombre = "Ileny", Precio = 20.085M},
+            new Alumnos {Matricula = 5, Nombre = "Karla", Precio = 19.789M},
+            new Alumnos {Matricula = 6, Nombre = "Casco",Precio = 25.453M},
+            new Alumnos {Matricula = 7, Nombre = "Mitzi", Precio = 22.233M},
+            new Alumnos {Matricula = 8, Nombre = "Isabel",Precio = 19.434M},
+            new Alumnos {Matricula = 9, Nombre = "Zuri", Precio = 18.456M},
+
+        };
+
+
+        //foreach (var nombre in Lista)
+        //{
+        //    Console.WriteLine(nombre);
+        //}
+
+        //Lista.Clear();
+        //Lista.Sort();
+
+        Lista = Lista.OrderBy(x => x.Precio).ToList();
+        //Lista = Lista.OrderBy(x => x.Edad).ToList();
+
+        for (int i = 0; i < Lista.Count; i++)
+        {
+
+            Console.WriteLine(Lista[i]);
+        }
+
+        #endregion
+        Console.ReadKey();
+    }
+}
